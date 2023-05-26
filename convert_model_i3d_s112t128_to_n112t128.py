13d28ed1d0c5e09484b7eeae7fb5a70d94bef924
Ruikang07 <55299854+Ruikang07@users.noreply.github.com>
2023-05-26 13:41:24 -0400

    Add files via upload

---
+++ convert_model_i3d_s112t128_to_n112t128.py
@@ -0,0 +1,33 @@
+# by Ruikang Luo
+import os
+import torch
+from i3d_src.utils import *
+from i3d_src.i3d_n112 import I3D as I3D112
+
+weight_path_i3dm112 = "weights/i3d_s112_k400_d1_f128_val_acc_0.6002.pth"
+n112 = "i3d_n112_k400_d1_f128_val_acc_0.6002.pth"
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+save_path = "./model_n112/"
+if not os.path.exists(save_path):
+    os.mkdir(save_path)   
+
+
+model112 = I3D112(num_classes=400, modality='rgb', dropout_prob=0)
+model112.to(device)  
+
+pre_trained = torch.load(weight_path_i3dm112)
+new_weights = list(pre_trained.items())
+my_model_kvpair = model112.state_dict()
+count=0
+for key,value in my_model_kvpair.items():
+    layer_name, weights = new_weights[count]      
+    #mymodel_kvpair[key]=weights.detach().clone()
+    my_model_kvpair[key] = weights
+    count+=1           
+#model112.load_state_dict(torch.load(weight_path_i3dm112))
+model112.load_state_dict(my_model_kvpair)
+
+tmp_model112_path = save_path+n112
+torch.save(model112.state_dict(), tmp_model112_path) 
